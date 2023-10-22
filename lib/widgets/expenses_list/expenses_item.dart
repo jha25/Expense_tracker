a0edd5cb95305bd64b8dcad16a135055f527a2d6
Ha <630019@bah.com>
2023-10-22 13:00:10 -0500

    first commit

---
+++ lib/widgets/expenses_list/expenses_item.dart
@@ -0,0 +1,16 @@
+import 'package:expense_tracker/models/expense.dart';
+import 'package:flutter/material.dart';
+
+class ExpenseItem extends StatelessWidget {
+  const ExpenseItem(this.expense, {super.key});
+
+  final Expense expense;
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Card(
+      child: Text(expense.title),
+    );
+  }
+}
