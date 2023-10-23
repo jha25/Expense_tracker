595db3846456cc3a3cb45c78fd96ed4daf656c71
Ha <630019@bah.com>
2023-10-23 12:26:11 -0500

    adding initial themes

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -44,15 +44,46 @@ class _ExpensesState extends State<Expenses> {
   }
 
   void _removedExpense(Expense expense) {
+    final expenseIndex = _registeredExpenses.indexOf(expense);
+
     setState(
       () {
         _registeredExpenses.remove(expense);
       },
     );
+
+    ScaffoldMessenger.of(context).clearSnackBars();
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        duration: const Duration(seconds: 3),
+        content: const Text('Expense deleted.'),
+        action: SnackBarAction(
+          label: 'Undo',
+          onPressed: () {
+            setState(
+              () {
+                _registeredExpenses.insert(expenseIndex, expense);
+              },
+            );
+          },
+        ),
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
+    Widget mainContent = const Center(
+      child: Text('No expenses found. Start adding some!'),
+    );
+
+    if (_registeredExpenses.isNotEmpty) {
+      mainContent = ExpensesList(
+        expenses: _registeredExpenses,
+        onRemoveExpense: _removedExpense,
+      );
+    }
+
     // TODO: implement build
     return Scaffold(
       appBar: AppBar(
@@ -68,10 +99,7 @@ class _ExpensesState extends State<Expenses> {
         children: [
           const Text('The chart'),
           Expanded(
-            child: ExpensesList(
-              expenses: _registeredExpenses,
-              onRemoveExpense: _removedExpense,
-            ),
+            child: mainContent,
           ),
         ],
       ),
