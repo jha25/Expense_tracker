ad47d1eb2c09851377f85f36a3e547fc25351d55
Ha <630019@bah.com>
2023-10-23 20:18:17 -0500

    adding orientation feature

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:expense_tracker/widgets/expenses.dart';
 
+// import 'package:flutter/services.dart';
+
 var kColorScheme = ColorScheme.fromSeed(
   seedColor: Color.fromARGB(255, 96, 59, 181),
 );
@@ -11,6 +13,10 @@ var kDarkColorScheme = ColorScheme.fromSeed(
 );
 
 void main() {
+  // WidgetsFlutterBinding.ensureInitialized();
+  // SystemChrome.setPreferredOrientations([
+  //   DeviceOrientation.portraitUp,
+  // ]).then((fn) {
   runApp(
     MaterialApp(
       darkTheme: ThemeData.dark().copyWith(
@@ -60,4 +66,5 @@ void main() {
       home: const Expenses(),
     ),
   );
+  // });
 }
